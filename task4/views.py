eec9ce31122cd34bc0d91f9cbe6a9bcceb223daa
KornachevaOlga <167702088+KornachevaOlga@users.noreply.github.com>
2024-12-15 19:36:31 +0300

    module_18

---
+++ task4/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def game_platform(request):
+    return render(request, 'fourth_task/platform.html')
+
+def game(request):
+    game_dict = {'games': ["Atomic Heart", "Cyberpunk 2077"]}
+    context = {'game_dict': game_dict}
+    return render(request, 'fourth_task/games.html', context)
+
+def cart(request):
+    return render(request, 'fourth_task/cart.html')
+
+# def base(request):
+#     return render(request, 'menu.html')
